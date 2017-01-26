e5c6511abf3e531421d7c494bc7183a6e28cb9a5
Koltunov Alexander <firstsano@gmail.com>
2017-01-26 17:54:47 +0300

    Add common inline helpers

--- frontend/components/helpers/inline_helpers.php
+++ frontend/components/helpers/inline_helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+use Yii;
+
 function get_base_class_name($object)
 {
     return (new \ReflectionClass($object))->getShortName();
@@ -8,4 +10,19 @@ function get_base_class_name($object)
 function camelcase_to_snake_case($string)
 {
     return strtolower(preg_replace('/([a-z])([A-Z])/', '$1-$2', $string));
+}
+
+function format_f($float)
+{
+    return Yii::$app->formatter->asDecimal($float, 2);
+}
+
+function currency()
+{
+    return Yii::$app->params['currency'];
+}
+
+function quantity($quantity)
+{
+    return Yii::t('site', 'test');
 }

a57a8fe683f8daf41cee287e92de8f40510705f6
Koltunov Alexander <firstsano@gmail.com>
2017-02-05 16:41:56 +0300

    Set up grunt for frontend css printing

---
+++ frontend/Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    "web/css/site.css": "views/scss/site.scss"
+                }
+            },
+        },
+        watch: {
+            sass: {
+                files: [
+                    'views/scss/*.scss',
+                    'views/scss/components/*.scss',
+                ],
+                tasks: ['sass'],
+                options: {
+                    livereload: true
+                }
+            },
+        }
+    });
+
+    // Plugin loading
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    // Task definition
+    grunt.registerTask('default', ['watch']);
+};

989618ac41f27496c5c4d17361219d6bccaa987a
Koltunov Alexander <firstsano@gmail.com>
2017-04-02 17:16:23 +0300

    Refactor product view to show images as gallery

--- frontend/components/widgets/views/product-image.php
+++ frontend/components/widgets/views/product-image.php
@@ -3,30 +3,31 @@
 /* @var $this \yii\web\View */
 /* @var $product \common\models\Product */
 
-use frontend\components\extensions\Html;
 use frontend\components\extensions\Url;
-
-\frontend\components\widgets\assets\ProductImageAsset::register($this);
-$activeImageUrl = $product->mainImage->url;
+use dosamigos\gallery\Gallery;
 
 ?>
 
 <div class="product-image">
-    <div class="product-image__main-image">
-        <?= Html::img(Url::toImage($product->mainImage->path, 'main'), [
-            'alt' => $product->name,
-            'class' => 'product-image__image'
-        ]) ?>
-    </div>
-    <div class="product-image__image-thumbnails">
-        <?php foreach($product->productImages as $image): ?>
-            <div class="product-image__thumbnail
-                <?= ($activeImageUrl === $image->url)? "product-image__thumbnail_active" : "" ?>">
-                <?= Html::img(Url::toImage($image->path, 'main'), [
-                    'alt' => $product->name,
+    <?php
+        $items = [];
+        foreach($product->productImages as $image) {
+            $items[] = [
+                'src' => Url::toImage($image->path, 'thumb'),
+                'url' => Url::toImage($image->path, 'main'),
+                'options' => [
+                    'class' => 'product-image__thumbnail'
+                ],
+                'imageOptions' => [
                     'class' => 'product-image__thumbnail-image'
-                ]) ?>
-            </div>
-        <?php endforeach; ?>
-    </div>
+                ]
+            ];
+        }
+        echo Gallery::widget([
+            'items' => $items,
+            'options' => [
+                'class' => 'product-image__image-thumbnails'
+            ]
+        ]);
+    ?>
 </div>

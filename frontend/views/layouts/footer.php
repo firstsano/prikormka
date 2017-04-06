e9e6073f2992440be6a2a132e8067df20e9d57ee
Koltunov Alexander <firstsano@gmail.com>
2017-04-06 18:57:31 +0300

    Remove fisherman

--- frontend/views/layouts/footer.php
+++ frontend/views/layouts/footer.php
@@ -5,7 +5,7 @@
 ?>
 
 <footer class="footer">
-    <?= $this->render('footer/info') ?>
+    <?//= $this->render('footer/info') ?>
     <?= $this->render('footer/sitemap') ?>
     <?= $this->render('footer/copyright-and-socials') ?>
 </footer>

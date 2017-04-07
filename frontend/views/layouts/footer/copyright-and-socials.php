95085efa91730e0ec414d5afa6d78c4131312169
Koltunov Alexander <firstsano@gmail.com>
2017-04-07 20:15:04 +0300

    Comment socials as unnecessary

--- frontend/views/layouts/footer/copyright-and-socials.php
+++ frontend/views/layouts/footer/copyright-and-socials.php
@@ -11,10 +11,10 @@ use yii\helpers\Html;
         <div class="footer-cas__copyright">
             &copy; 2017 Все права защищены.
         </div>
-        <div class="footer-cas__socials">
-            <?= Html::img('@icons/twitter.png', ['alt' => 'Twitter', 'class' => 'footer-cas__social']) ?>
-            <?= Html::img('@icons/instagram.png', ['alt' => 'Instagram', 'class' => 'footer-cas__social']) ?>
-            <?= Html::img('@icons/facebook.png', ['alt' => 'Facebook', 'class' => 'footer-cas__social']) ?>
-        </div>
+<!--        <div class="footer-cas__socials">-->
+<!--            --><?//= Html::img('@icons/twitter.png', ['alt' => 'Twitter', 'class' => 'footer-cas__social']) ?>
+<!--            --><?//= Html::img('@icons/instagram.png', ['alt' => 'Instagram', 'class' => 'footer-cas__social']) ?>
+<!--            --><?//= Html::img('@icons/facebook.png', ['alt' => 'Facebook', 'class' => 'footer-cas__social']) ?>
+<!--        </div>-->
     </div>
 </div>

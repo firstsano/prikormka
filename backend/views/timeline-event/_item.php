7ba53f2af4b5d8a81088aeee5da168fcb500daea
Koltunov Alexander <firstsano@gmail.com>
2017-04-21 17:36:05 +0300

    Fix relative time bug

--- backend/views/timeline-event/_item.php
+++ backend/views/timeline-event/_item.php
@@ -7,7 +7,7 @@
 <div class="timeline-item">
     <span class="time">
         <i class="fa fa-clock-o"></i>
-        <?php echo Yii::$app->formatter->asRelativeTime($model->created_at) ?>
+        <?php echo Yii::$app->formatter->asDatetime($model->data['created_at']) ?>
     </span>
     <h3 class="timeline-header">
         <?php echo Yii::t('backend', 'You have new event') ?>
